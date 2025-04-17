01ca1423459d5dd38d1ea231763b70c31092560a
Tom Watson <tom@tomwatson.qa>
2025-04-17 12:48:21 +0100

    init terraform

---
+++ infrastructure/terraform.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  cloud {
+    organization = "tomwatsonqa"
+
+    workspaces {
+      name = "whatsapp-ai-chatbot"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      Project       = "whatsapp-ai-chatbot"
+      ProvisionedBy = "terraform"
+    }
+  }
+}
